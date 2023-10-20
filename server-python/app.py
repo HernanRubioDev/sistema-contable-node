3245a902c5705e6091a2d98e707ad8fab8ad552e
JuanSobron01 <sobronjuancruz@gmail.com>
2023-10-20 19:01:57 -0300

    ADD rollback en inserts

--- server-python/app.py
+++ server-python/app.py
@@ -47,177 +47,202 @@ def login():
 ############## Asientos Contables ############
 @app.post('/movements')
 def done_move ():
-    #Traigo los datos del json que recibo 
-    asiento = request.json
-    #import pdb; pdb.set_trace()
-    #Abro conexion con la base de datos para hacer selects de los datos que necesito
-    # 1: Necesito hacer una busqueda de la cuenta para obtener el id
-    conn = conection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-    lineas_asiento = asiento["rows"]
-    asiento_balanceado = True #Inicializo variable para ver tema balanceo
-    move_insert = 0
-    id_asiento = 0
-    if len(lineas_asiento) < 2: 
-        return jsonify({
-                "status":400,
-                "title":"Error",
-                "error":'Cantidad de lineas menor a 2',
-                "success":False
-                })
-    for linea in lineas_asiento:
-        date = linea.get('date')
-        account = linea.get('account')
-        monto = linea.get('ammount')
-        type = linea.get('type')
-        id_cpny  = linea.get('id_company')
-        move_number = asiento.get('moveNum')
-        description = asiento.get('description')
-        date = datetime.strptime(date, '%Y-%m-%d').date() #Datetime para comparar despues
-        #Busco el id de la cuenta 
-        cur.execute("SELECT id_account,credit,code from accounts where name = %s",(account,))
-        try:
-            #import pdb; pdb.set_trace()
-            row = cur.fetchone()
-            id_account = row['id_account']
-            credito_cuenta = row['credit']
-            # Me traigo el tipo de cuenta
-            tipo_cuenta = row['code']
-            #Me fijo por codigo que tipo de cuenta es
-            if tipo_cuenta[0] == '1':
-                tipo_cuenta = 'activo'
-            
-            if tipo_cuenta[0] == '2':
-                tipo_cuenta = 'pasivo'
-            
-            if tipo_cuenta[0] == '3':
-                tipo_cuenta = 'patrimonio'
 
-            if tipo_cuenta[0] == '4':
-                tipo_cuenta = 'r+'
-
-            if tipo_cuenta[0] == '5':
-                tipo_cuenta = 'r-'
-            
-            # Si es activo y el tipo de linea es 'HABER' tiene que restar el saldo
-
-            if type == 'haber' and tipo_cuenta == 'activo':
-                total = credito_cuenta - Decimal(monto)
-                if total <= 0:
-                    return jsonify({
-                        "status":400,
-                        "title":"Error",
-                        "body":('El saldo de la cuenta %s es menor al monto a insertar en la linea',(account,)),
-                        "success":False
+    try:
+        import pdb; pdb.set_trace()
+        #Traigo los datos del json que recibo 
+        asiento = request.json
+        #import pdb; pdb.set_trace()
+        #Abro conexion con la base de datos para hacer selects de los datos que necesito
+        # 1: Necesito hacer una busqueda de la cuenta para obtener el id
+        conn = conection()
+        #conn.autocommit = False
+        conn.set_session(autocommit=False)
+        cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+        #conn.begin()
+        lineas_asiento = asiento["rows"]
+        asiento_balanceado = True #Inicializo variable para ver tema balanceo
+        move_insert = 0
+        id_asiento = 0
+        if len(lineas_asiento) < 2: 
+            return jsonify({
+                    "status":400,
+                    "title":"Error",
+                    "error":'Cantidad de lineas menor a 2',
+                    "success":False
                     })
+        for linea in lineas_asiento:
+            date = linea.get('date')
+            account = linea.get('account')
+            monto = linea.get('ammount')
+            type = linea.get('type')
+            id_cpny  = linea.get('id_company')
+            move_number = asiento.get('moveNum')
+            description = asiento.get('description')
+            date = datetime.strptime(date, '%Y-%m-%d').date() #Datetime para comparar despues
+            #Busco el id de la cuenta 
+            cur.execute("SELECT id_account,credit,code from accounts where name = %s",(account,))
+            try:
+                #import pdb; pdb.set_trace()
+                row = cur.fetchone()
+                id_account = row['id_account']
+                credito_cuenta = row['credit']
+                # Me traigo el tipo de cuenta
+                tipo_cuenta = row['code']
+                #Me fijo por codigo que tipo de cuenta es
+                if tipo_cuenta[0] == '1':
+                    tipo_cuenta = 'activo'
                 
-            #Si es pasivo y el tipo es DEBE resta
-            if type == 'debe' and tipo_cuenta == 'pasivo':
-                total = credito_cuenta - Decimal(monto)
-                if total <= 0: 
-                    return jsonify({
-                        "status":400,
-                        "title":"Error",
-                        "body":('El saldo de la cuenta %s es menor al monto a insertar en la linea',(account,)),
-                        "success":False
-                    })
+                if tipo_cuenta[0] == '2':
+                    tipo_cuenta = 'pasivo'
+                
+                if tipo_cuenta[0] == '3':
+                    tipo_cuenta = 'patrimonio'
+
+                if tipo_cuenta[0] == '4':
+                    tipo_cuenta = 'r+'
+
+                if tipo_cuenta[0] == '5':
+                    tipo_cuenta = 'r-'
+                
+                # Si es activo y el tipo de linea es 'HABER' tiene que restar el saldo
+
+                if type == 'haber' and tipo_cuenta == 'activo':
+                    total = credito_cuenta - Decimal(monto)
+                    if total <= 0:
+                        return jsonify({
+                            "status":400,
+                            "title":"Error",
+                            "body":('El saldo de la cuenta %s es menor al monto a insertar en la linea',(account,)),
+                            "success":False
+                        })
+                    
+                #Si es pasivo y el tipo es DEBE resta
+                if type == 'debe' and tipo_cuenta == 'pasivo':
+                    total = credito_cuenta - Decimal(monto)
+                    if total <= 0: 
+                        return jsonify({
+                            "status":400,
+                            "title":"Error",
+                            "body":('El saldo de la cuenta %s es menor al monto a insertar en la linea',(account,)),
+                            "success":False
+                        })
 
 
-        except:
-            return jsonify({
-                "status":400,
-                "title":"Error",
-                "body":('La cuenta %s no existe',(account,)),
-                "success": False
-            })
-        #Monto no puede ser menor que 0
-        if float(monto) <= 0 : 
-            return jsonify({
-                "status":400,
-                "title":"Error",
-                "body":'El monto debe ser mayor',
-                "success":False
-            })
-        
-        #Credit & debit
-        debe = 0
-        haber = 0
-        if type == 'debe' :debe+=float(monto)
-        if type == 'haber': haber += float(monto)
-        #Obtengo la ultima fecha del asiento
-
-        cur.execute("SELECT MAX(move_date) from accounts_moves") #Me traigo la fecha mas alta que es la que voy a necesitar para los asientos
-        
-        fecha_ultimo = cur.fetchone()['max']
-        # Restriccion/validacion 1 : fecha del asiento no puede ser menor a la del ultimo asiento
-        if fecha_ultimo is not None:
-            if fecha_ultimo > date : 
+            except:
                 return jsonify({
                     "status":400,
                     "title":"Error",
-                    "error":'Fecha erronea',
+                    "body":('La cuenta %s no existe',(account,)),
+                    "success": False
+                })
+            #Monto no puede ser menor que 0
+            if float(monto) <= 0 : 
+                return jsonify({
+                    "status":400,
+                    "title":"Error",
                     "body":'El monto debe ser mayor',
                     "success":False
                 })
-        
-        else:
-            fecha_ultimo = None
-        
-        #Validacion de saldos de cuentas
-        
-        if not validar_balance(lineas_asiento):
-            print("asiento no valanciado")
-            asiento_balanceado = False
-            break
-        
-        elif validar_balance(lineas_asiento) : #Asiento balanceado y no se realizo insercion
-            if move_insert == 0:
-                try:
-                    cur.execute("INSERT into accounts_moves(move_date,description) values (%s,%s) RETURNING id_move",(date,description))
-                    conn.commit()
-                    if cur.rowcount == 1:
-                        move_insert += 1
-                        print('Insercion exitosa')
-                        id_insertado = cur.fetchone()['id_move']
-                        id_asiento = id_insertado
-                        #Insercion primera linea
-                        cur.execute("INSERT into accounts_moves_lines(id_move,id_account,date,debit,credit) values (%s,%s,%s,%s,%s)",(int(id_asiento),int(id_account),date,debe,haber))
-                        conn.commit()
-                except:
-                    return jsonify({
-                        "status":400,
-                        "title":"Error",
-                        "body":'No fue posible insertar el asiento',
-                        "success":False
-                    })
-            else:
-                #Insercion de linea
-                try:
-                    cur.execute("INSERT into accounts_moves_lines(id_move,id_account,date,debit,credit) values (%s,%s,%s,%s,%s)",(int(id_asiento),int(id_account),date,debe,haber))
-                    conn.commit()
-                except:
-                    return jsonify({
-                        "status":400,
-                        "title":"Error",
-                        "body":'No fue posible insertar las lineas correspondientes al asiento',
-                        "success":False
-                    })
+            
+            #Credit & debit
+            debe = 0
+            haber = 0
+            if type == 'debe' :debe+=float(monto)
+            if type == 'haber': haber += float(monto)
+            #Obtengo la ultima fecha del asiento
 
-    if asiento_balanceado: 
-        print('Asiento Balanceado')
-        return jsonify({
-            "status" : 201,
-            "title": "Agregado",
-            "body": 'El asiento se inserto correctamente',
-            "success":True
-        })
-    else:
+            cur.execute("SELECT MAX(move_date) from accounts_moves") #Me traigo la fecha mas alta que es la que voy a necesitar para los asientos
+            
+            fecha_ultimo = cur.fetchone()['max']
+            # Restriccion/validacion 1 : fecha del asiento no puede ser menor a la del ultimo asiento
+            if fecha_ultimo is not None:
+                if fecha_ultimo > date : 
+                    return jsonify({
+                        "status":400,
+                        "title":"Error",
+                        "error":'Fecha erronea',
+                        "body":'El monto debe ser mayor',
+                        "success":False
+                    })
+            
+            else:
+                fecha_ultimo = None
+            
+            #Validacion de saldos de cuentas
+            
+            if not validar_balance(lineas_asiento):
+                print("asiento no valanciado")
+                asiento_balanceado = False
+                break
+            
+            elif validar_balance(lineas_asiento) : #Asiento balanceado y no se realizo insercion
+                if move_insert == 0:
+                    try:
+                        cur.execute("INSERT into accounts_moves(move_date,description) values (%s,%s) RETURNING id_move",(date,description))
+                        #conn.commit()
+                        if cur.rowcount == 1:
+                            move_insert += 1
+                            print('Insercion exitosa')
+                            id_insertado = cur.fetchone()['id_move']
+                            id_asiento = id_insertado
+                            #Insercion primera linea
+                            cur.execute("INSERT into accounts_moves_lines(id_move,id_account,date,debit,credit) values (%s,%s,%s,%s,%s)",(int(id_asiento),int(id_account),date,debe,haber))
+                            #conn.commit()
+                    except:
+                        return jsonify({
+                            "status":400,
+                            "title":"Error",
+                            "body":'No fue posible insertar el asiento',
+                            "success":False
+                        })
+                else:
+                    #Insercion de linea
+                    try:
+                        cur.execute("INSERT into accounts_moves_lines(id_move,id_account,date,debit,credit) values (%s,%s,%s,%s,%s)",(int(id_asiento),int(id_account),date,debe,haber))
+                        #conn.commit()
+                    except:
+                        return jsonify({
+                            "status":400,
+                            "title":"Error",
+                            "body":'No fue posible insertar las lineas correspondientes al asiento',
+                            "success":False
+                        })
+        conn.commit()
+
+        if asiento_balanceado: 
+            print('Asiento Balanceado')
+            return jsonify({
+                "status" : 201,
+                "title": "Agregado",
+                "body": 'El asiento se inserto correctamente',
+                "success":True
+            })
+        else:
+            return jsonify({
+                "status": 400,
+                "title":"Error",
+                "body": 'El asiento no está balanceado.',
+                "success":False
+            })
+    except Exception as e:
+        # En caso de un error, revertir la transacción
+        conn.rollback()
+        print("Error:", e)
         return jsonify({
             "status": 400,
-            "title":"Error",
-            "body": 'El asiento no está balanceado.',
-            "success":False
+            "title": "Error",
+            "body": 'Ocurrió un error al procesar el asiento.',
+            "success": False
         })
+    finally:
+        # Restablecer la configuración de autocommit
+        #conn.autocommit = True
+        conn.set_session(autocommit=True)
+        # Cerrar el cursor y la conexión
+        cur.close()
+        conn.close()
+
 
 
 #Funcion para validar asientos
