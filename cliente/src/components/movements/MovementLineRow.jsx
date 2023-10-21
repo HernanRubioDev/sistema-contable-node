450828b56b414c8263981419c04c33e28cdac0db
JuanSobron01 <sobronjuancruz@gmail.com>
2023-10-21 09:49:18 -0300

    ADD changes en vistas

--- cliente/src/components/movements/MovementLineRow.jsx
+++ cliente/src/components/movements/MovementLineRow.jsx
@@ -2,9 +2,9 @@ const MovementLineRow = ({data})=>{
 		const {name, credit, debit}=data
     return(
       <tr scope="row">
-        <td className={debit !== null ? 'text-start':'text-end'}>{name}</td>
-        <td className={debit !== null ? 'text-start':'text-end'}>{debit}</td>
-        <td className={debit !== null ? 'text-start':'text-end'}>{credit}</td>
+        <td className={debit !== "0.00" ? 'text-start':'text-end'}>{name}</td>
+        <td className={debit !== "0.00" ? 'text-start':'text-end'}>{debit === "0.00" ? '-' : debit}</td>
+        <td className={debit !== "0.00" ? 'text-start':'text-end'}>{credit === "0.00" ? '-' : credit}</td>
       </tr>
     );
 }
