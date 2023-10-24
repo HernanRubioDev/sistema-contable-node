1ddf17011ae5b8f949056f3f2be92d7cd5b699ea
Hernan Rubio <hernan.andres.rubio@gmail.com>
2023-10-24 16:09:12 -0300

    bdd update

--- cliente/src/hooks/useAccount.js
+++ cliente/src/hooks/useAccount.js
@@ -31,8 +31,10 @@ const useAccount = ()=>{
 
     try {
       const res = await api.post(url, options);
+      console.log(res)
       switch (true) {
         case res.status === 201:
+          console.log("entro")
           setResponse(res)
           infoToast.show();
           break;
@@ -63,6 +65,7 @@ const useAccount = ()=>{
       }
       
     } catch (error) {
+      console.log(error)
       setResponse({status:res.status, title:"Error", body:"La cuenta no se ha podido crear."})
       infoToast.show();
     }
