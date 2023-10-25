e7f6ec786b7d141bdefa20e0a6d9efa3eb5ad68f
Hernan Rubio <hernan.andres.rubio@gmail.com>
2023-10-24 22:42:43 -0300

    movement validations added

--- server/src/middlewares/movementMiddleware.js
+++ server/src/middlewares/movementMiddleware.js
@@ -2,7 +2,7 @@ const { movementValidator } = require("../validators/movementValidator")
 
 const movementMiddleware = (req, res, next)=>{
  const validations = {...movementValidator(req.body)}
- Object.keys(validations).length !== 0 ? res.json({status:400, title:"Error", body: validations.message, success: false}) : console.log("next");
+ Object.keys(validations).length !== 0 ? res.json({status:400, title:"Error", body: validations.message, success: false}) : next();
 }
 
 module.exports={movementMiddleware}
